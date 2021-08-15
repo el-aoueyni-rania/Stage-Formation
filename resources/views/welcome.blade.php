53e358199cd98bfaf02cb7a252b0f75ff321245e
el-aoueyni-rania <elaoueynirania@gmail.com>
2021-08-15 02:18:02 +0200

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Formation
                 </div>
 
                 <div class="links">
