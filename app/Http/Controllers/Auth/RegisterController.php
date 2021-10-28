99a9227cbabd76bfee09dceb3347e2c45c8b3739
el-aoueyni-rania <elaoueynirania@gmail.com>
2021-10-28 04:23:24 +0200

    modification final

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'numeric', 'min:0', 'max:1'],
+            'role' => ['required', 'string'],
         ]);
     }
 
